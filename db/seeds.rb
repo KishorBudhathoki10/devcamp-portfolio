9ec54ae38095c1a0ea6e558d2fc48c80262f6d38
Kishor Budhathoki <pinkfloyd@PINKFLOYDs-MacBook-Pro.local>
2020-02-06 13:23:26 +0100

    Integrated index action for portfolio items.

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,8 +21,8 @@ puts "5 skills created."
     title: "Portfolio title: #{portfolio_item}",
     subtitle: "My great service",
     body: "The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from de Finibus Bonorum et Malorum by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.",
-    main_image: "http://placehold.it/600*400",
-    thumb_image: "http://placehold.it/350*200"
+    main_image: "https://via.placeholder.com/600x400.png",
+    thumb_image: "https://via.placeholder.com/350x200.png"
   )
 end
 
