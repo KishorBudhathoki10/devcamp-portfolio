f40e057992c731ac98cdae5085da054f6ee97c0b
Kishor Budhathoki <pinkfloyd@PINKFLOYDs-MacBook-Pro.local>
2020-02-24 17:09:13 +0100

    Implemented partials for blog, font-awesome and pagination

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -1,2 +1,16 @@
 module BlogsHelper
+  def quotes
+    [
+      "“You might not think that programmers are artists, but programming is an extremely creative profession. It's logic-based creativity.“ - John Romero",
+      "“Every programmer is an author.” - Sercan Leylek",
+      "“The 50-50-90 rule: Anytime you have a 50-50 chance of getting something right, there's a 90% probability you'll get it wrong.” - Anonymous",
+      "“In software, the most beautiful code, the most beautiful functions, and the most beautiful programs are sometimes not there at all.” - Jon Bentley",
+      "“There are 10 types of people in this world, those who understand binary and those who don't.” - Anonymous",
+      "“One of my most productive days was throwing away 1000 lines of code.” - Ken Thompson",
+      "“Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live” - John Woods",
+      "“A code is like love, it has created with clear intentions at the beginning, but it can get complicated.” - Gerry Geek",
+      "“Code never lies, comments sometimes do.” - Ron Jeffries",
+      "“Any code of your own that you haven’t looked at for six or more months might as well have been written by someone else.” - (Eagleson’s Law)"
+    ].sample
+  end
 end
